40f3348ad60ae83624680519db3b7913519a746f
hieu4399243 <hieu243268@gmail.com>
2023-05-22 21:50:21 +0700

    setup componnet

---
+++ wallet-native/Screens/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Image,
+    TextInput,
+    Modal,
+    FlatList,
+    KeyboardAvoidingView,
+    ScrollView,
+    Platform
+} from "react-native";
+
+
+export default function Home() {
+  return (
+    <View>
+      <Text>SignUp</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ wallet-native/Screens/SignUp.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Image,
+    TextInput,
+    Modal,
+    FlatList,
+    KeyboardAvoidingView,
+    ScrollView,
+    Platform
+} from "react-native";
+
+
+export default function SignUp() {
+  return (
+    <View>
+      <Text>SignUp</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
