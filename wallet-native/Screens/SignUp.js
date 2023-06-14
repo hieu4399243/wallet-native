55e5504faf32ef99549e1b267508ba80dcc249c9
hieu4399243 <hieu243268@gmail.com>
2023-06-14 09:11:55 +0700

    code, image, by country

--- wallet-native/Screens/SignUp.js
+++ wallet-native/Screens/SignUp.js
@@ -17,10 +17,37 @@ import { LinearGradient } from "expo-linear-gradient";
 import { COLORS, SIZES, FONTS, icons, images } from "../constansts";
 
 export default function SignUp() {
-  
-  const [showPassword, setShowPassword] = React.useState(false)
-  
-  
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const [areas, setAreas] = React.useState([]);
+  const [selectedArea, setSelectedArea] = React.useState(null);
+  const [modalVisible, setModalVisible] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        let areaData = data.map((item) => {
+          return {
+            code: item.cca2,
+            name: item.name,
+            callingCode: `+${item.idd.root[0]}`,
+            flag: `https://flagcdn.com/w320/${item.cca2}.png`,
+          };
+        });
+
+        setAreas(areaData);
+
+        if (areaData.length > 0) {
+          let defaultData = areaData.filter((a) => a.code == "US");
+
+          if (defaultData.length > 0) {
+            setSelectedArea(defaultData[0]);
+          }
+        }
+      });
+  }, []);
+
   function renderHeader() {
     return (
       <TouchableOpacity
@@ -120,7 +147,7 @@ export default function SignUp() {
                 flexDirection: "row",
                 ...FONTS.body2,
               }}
-              onPress={() => alert("hieu")}
+              onPress={() => setModalVisible(true)}
             >
               <View style={{ justifyContent: "center" }}>
                 <Image
@@ -135,7 +162,7 @@ export default function SignUp() {
 
               <View style={{ justifyContent: "center", marginLeft: 5 }}>
                 <Image
-                  source={images.usFlag}
+                  source={{ uri: selectedArea?.flag }}
                   resizeMode="contain"
                   style={{
                     width: 30,
@@ -144,8 +171,10 @@ export default function SignUp() {
                 />
               </View>
 
-              <View style={{ justifyContent: 'center', marginLeft: 5 }}>
-                  <Text style={{ color: COLORS.white, ...FONTS.body3 }}>US+1</Text>
+              <View style={{ justifyContent: "center", marginLeft: 5 }}>
+                <Text style={{ color: COLORS.white, ...FONTS.body3 }}>
+                  US+1
+                </Text>
               </View>
             </TouchableOpacity>
 
@@ -168,63 +197,119 @@ export default function SignUp() {
         </View>
         {/* Password */}
         <View style={{ marginTop: SIZES.padding * 2 }}>
-        <Text style={{ color: COLORS.lightGreen, ...FONTS.body3 }}>Password</Text>
-        <TextInput
+          <Text style={{ color: COLORS.lightGreen, ...FONTS.body3 }}>
+            Password
+          </Text>
+          <TextInput
             style={{
-                marginVertical: SIZES.padding,
-                borderBottomColor: COLORS.white,
-                borderBottomWidth: 1,
-                height: 40,
-                color: COLORS.white,
-                ...FONTS.body3
+              marginVertical: SIZES.padding,
+              borderBottomColor: COLORS.white,
+              borderBottomWidth: 1,
+              height: 40,
+              color: COLORS.white,
+              ...FONTS.body3,
             }}
             placeholder="Enter Password"
             placeholderTextColor={COLORS.white}
             selectionColor={COLORS.white}
             secureTextEntry={!showPassword}
-        />
-        <TouchableOpacity
+          />
+          <TouchableOpacity
             style={{
-                position: 'absolute',
-                right: 0,
-                bottom: 10,
-                height: 30,
-                width: 30
+              position: "absolute",
+              right: 0,
+              bottom: 10,
+              height: 30,
+              width: 30,
             }}
             onPress={() => setShowPassword(!showPassword)}
-        >
+          >
             <Image
-                source={showPassword ? icons.disable_eye : icons.eye}
-                style={{
-                    height: 20,
-                    width: 20,
-                    tintColor: COLORS.white
-                }}
+              source={showPassword ? icons.disable_eye : icons.eye}
+              style={{
+                height: 20,
+                width: 20,
+                tintColor: COLORS.white,
+              }}
             />
-        </TouchableOpacity>
-    </View>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 
   function renderButton() {
     return (
-        <View style={{ margin: SIZES.padding * 3 }}>
-            <TouchableOpacity
-                style={{
-                    height: 60,
-                    backgroundColor: COLORS.black,
-                    borderRadius: SIZES.radius / 1.5,
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}
-                //onPress={() => navigation.navigate("Home")}
+      <View style={{ margin: SIZES.padding * 3 }}>
+        <TouchableOpacity
+          style={{
+            height: 60,
+            backgroundColor: COLORS.black,
+            borderRadius: SIZES.radius / 1.5,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          //onPress={() => navigation.navigate("Home")}
+        >
+          <Text style={{ color: COLORS.white, ...FONTS.h3 }}>Continue</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  function renderAreaCodesModal() {
+    const renderItem = ({ item }) => {
+      return (
+        <TouchableOpacity
+          style={{ padding: SIZES.padding, flexDirection: "row" }}
+          onPress={() => {
+            setSelectedArea(item);
+            setModalVisible(false);
+          }}
+        >
+          <Image
+            source={{ uri: item.flag }}
+            style={{
+              width: 30,
+              height: 30,
+              marginRight: 10,
+            }}
+          />
+          <Text style={{ ...FONTS.body4 }}>{item.name}</Text>
+        </TouchableOpacity>
+      );
+    };
+
+    return (
+      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+        <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+          <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+          >
+            <View
+              style={{
+                height: 400,
+                width: SIZES.width * 0.8,
+                backgroundColor: COLORS.lightGreen,
+                borderRadius: SIZES.radius,
+              }}
             >
-                <Text style={{ color: COLORS.white, ...FONTS.h3 }}>Continue</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+              <FlatList
+                data={areas}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.code}
+                showsVerticalScrollIndicator={false}
+                style={{
+                  padding: SIZES.padding * 2,
+                  marginBottom: SIZES.padding * 2,
+                }}
+              />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+    );
+  }
 
   return (
     <KeyboardAvoidingView
@@ -242,6 +327,7 @@ export default function SignUp() {
           {renderButton()}
         </ScrollView>
       </LinearGradient>
+      {renderAreaCodesModal()}
     </KeyboardAvoidingView>
   );
 }
